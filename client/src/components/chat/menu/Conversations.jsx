6da8e5d91993f48f22efef46b128a06214a866d6
Anubhav-Joria <anubhavjoria29@gmail.com>
2023-08-26 13:29:56 +0530

    formatting

--- client/src/components/chat/menu/Conversations.jsx
+++ client/src/components/chat/menu/Conversations.jsx
@@ -1,60 +1,61 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from "react";
 
-import { Box, styled, Divider } from '@mui/material';
+import { Box, styled, Divider } from "@mui/material";
 
-import { AccountContext } from '../../../context/AccountProvider';
+import { AccountContext } from "../../../context/AccountProvider";
 
 //components
-import Conversation from './Conversation';
-import { getUsers } from '../../../service/api';
+import Conversation from "./Conversation";
+import { getUsers } from "../../../service/api";
 
 const Component = styled(Box)`
-    overflow: overlay;
-    height: 81vh;
+  overflow: overlay;
+  height: 81vh;
 `;
 
 const StyledDivider = styled(Divider)`
-    margin: 0 0 0 70px;
-    background-color: #e9edef;
-    opacity: .6;
+  margin: 0 0 0 70px;
+  background-color: #e9edef;
+  opacity: 0.6;
 `;
 
 const Conversations = ({ text }) => {
-    const [users, setUsers] = useState([]);
-    
-    const { account, socket, setActiveUsers } = useContext(AccountContext);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            let data = await getUsers();
-            let fiteredData = data.filter(user => user.name.toLowerCase().includes(text.toLowerCase()));
-            setUsers(fiteredData);
-        }
-        fetchData();
-    }, [text]);
+  const { account, socket, setActiveUsers } = useContext(AccountContext);
 
-    useEffect(() => {
-        socket.current.emit('addUser', account);
-        socket.current.on("getUsers", users => {
-            setActiveUsers(users);
-        })
-    }, [account])
+  useEffect(() => {
+    const fetchData = async () => {
+      let data = await getUsers();
+      let fiteredData = data.filter((user) =>
+        user.name.toLowerCase().includes(text.toLowerCase()),
+      );
+      setUsers(fiteredData);
+    };
+    fetchData();
+  }, [text]);
 
-    return (
-        <Component>
-            {
-                users && users.map((user, index) => (
-                    user.sub !== account.sub && 
-                        <>
-                            <Conversation user={user} />
-                            {
-                                users.length !== (index + 1)  && <StyledDivider />
-                            }
-                        </>
-                ))
-            }
-        </Component>
-    )
-}
+  useEffect(() => {
+    socket.current.emit("addUser", account);
+    socket.current.on("getUsers", (users) => {
+      setActiveUsers(users);
+    });
+  }, [account]);
+
+  return (
+    <Component>
+      {users &&
+        users.map(
+          (user, index) =>
+            user.sub !== account.sub && (
+              <>
+                <Conversation user={user} />
+                {users.length !== index + 1 && <StyledDivider />}
+              </>
+            ),
+        )}
+    </Component>
+  );
+};
 
 export default Conversations;
